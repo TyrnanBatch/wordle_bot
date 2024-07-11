aeb3682ffd9745c2456842c6e6742edd066380cc
tyrnanbatch <tyrnan.batch@gmail.com>
2024-07-11 13:12:34 +0000

    Mocked up the two main functions

---
+++ index.js
@@ -0,0 +1,21 @@
+let currentInfomation = {}
+
+const getCurrentBestGuessForWin = () => {
+	// Iterate through list of remaining five letter words
+	// Note line number of ones that match criteria of current information
+	// Now have a list of current best guess' for win
+	// Every item in the list theoreticaly has the same chance of winning
+	// give top  most commonly used words to user?
+
+	return 0
+}
+
+const getCurrentBestGuessForInfo = () => {
+	// Iterate through list of remaining five letter words
+	// Give each letter that has not been guessed a score as to its frequncy in remaining words
+	// give each orange letter a change of it being it a certain place in the string
+	// give each words number based on how usful it will be.
+	// Order words and give to 5 to user
+
+	return 0
+}
